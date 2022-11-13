24b22f5be8f2f3d33ba98705ec8a91d12703761e
hyhpb <109414753+hyhpb@users.noreply.github.com>
2022-11-13 09:30:54 +0800

    Update 猜数字游戏.py

--- 猜数字游戏.py
+++ 猜数字游戏.py
@@ -1,33 +1,33 @@
-import time,sys
+import time,sys,os
 from random import*
 def pr(a):
     for i in a:
         sys.stdout.write(i)
         sys.stdout.flush()
         time.sleep(0.08)
-pr('猜数字游戏')
-print()
-pr('请输入猜数字最小范围：')
-snum = input()
-pr('请输入猜数字最大范围：')
-bnum = input()
-num  = randint(int(snum),int(bnum))
-num = str(num)
+pr('请输入猜数字的最小范围：')
+small = int(input())
+pr('请输入猜数字的最大范围：')
+big = int(input())
+num = randint(small,big)
 t = 0
 while True:
     pr('请输入你猜的数字：')
-    cnum = input()
-    if cnum != num:
-        if cnum > num:
-            pr('猜大了')
-            print()
-            t = t + 1
-        if cnum < num:
-            pr('猜小了')
-            print()
-            t = t + 1
-    if cnum == num:
-        pr('猜对了，你猜了'+str(t)+"次")
+    gnum = int(input())
+    if gnum == num:
+        t += 1
+        pr('猜对了，你猜了'+str(t)+'次')
         break
-#By HYH
-#2022.10.23
+    if gnum < num:
+        t += 1
+        pr('猜小了')
+        time.sleep(1)
+        os.system('cls')
+    if gnum > num:
+        t +=1
+        pr('猜大了')
+        time.sleep(1)
+        os.system('cls')
+#2022/11/13
+#by hyh
+#版本号：1.5
